3b63df818d365b39c6b708290558bf98b620e1f3
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-09-17 18:49:48 +0530

    newexpense module

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -6,7 +6,7 @@ export default function Expenses(props) {
   return (
     <Card className="expenses">
       {props.items.map((e) => (
-        <ExpenseItem title={e.title} amt={e.amount} date={e.date} />
+        <ExpenseItem key={e.id} title={e.title} amt={e.amount} date={e.date} />
       ))}
     </Card>
   );
---
+++ src/components/NewExpenses/NewExpense.jsx
@@ -0,0 +1,11 @@
+import "./NewExpense.css";
+import Form from "./ExpenseForm";
+
+const NewExpense = () => {
+  return (
+    <div>
+      <Form></Form>
+    </div>
+  );
+};
+export default NewExpense;
