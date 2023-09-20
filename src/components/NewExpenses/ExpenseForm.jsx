e1d83c622a69a7d8d0a97bb3d53d7a25793b486b
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-09-20 10:06:45 +0530

    form

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -1,20 +1,86 @@
 import "./ExpenseForm.css";
-
+import { useState } from "react";
 const ExpenseForm = () => {
+  const [newTitle, setNewTitle] = useState("");
+  const [newAmt, setNewAmt] = useState("");
+  const [newDate, setNewDate] = useState("");
+
+  //   const [userInput, setUserInput] = useState({
+  //     newTitle: "",
+  //     newAmt: "",
+  //     newDate: "",
+  //   });
+
+  const titleChangeHandler = (e) => {
+    // best way to handle
+    setNewTitle(e.target.value);
+
+    // 2nd type might give some problem when multiple states are changesd at same time , states are scheduled and if the other states aren't updated before new state is updated it might be a problem
+    // setUserInput({
+    //   ...userInput,
+    //   newTitle: e.target.value,
+    // });
+
+    // 3rd type for ensuring that state schedules in 2nd type are handles in this case
+    // setUserInput((prevState)=>{
+    //     ...prevState,
+    //     newTitle: e.target.value,
+    //   });
+  };
+  const amountChangeHandler = (e) => {
+    setNewAmt(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   newAmt: e.target.value,
+    // });
+  };
+  const dateChangeHandler = (e) => {
+    setNewDate(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   newDate: e.target.value,
+    // });
+  };
+
+  //   const inputChangeHandler = (identifier, value) => {
+  //     if (identifier === "title") {
+  //       setNewTitle(value);
+  //     } else if (identifier === "date") {
+  //       setNewDate(value);
+  //     } else {
+  //       setNewAmt(value);
+  //     }
+  //   };
   return (
     <form action="">
       <div className="new-expense_controls">
         <div className="new-expense_ctrl">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
+          {/* <input
+            type="text"
+            onChange={(event) =>
+              inputChangeHandler("title", event.target.value)
+            }
+          /> */}
         </div>
         <div className="new-expense_ctrl">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            onChange={amountChangeHandler}
+            type="number"
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-expense_ctrl">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2025-12-31" />
+          <input
+            onChange={dateChangeHandler}
+            type="date"
+            min="2022-01-01"
+            max="2025-12-31"
+          />
         </div>
         <div className="new-expense_action">
           <button type="submit">Add Expense</button>
